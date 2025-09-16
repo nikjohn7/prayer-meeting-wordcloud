a90ddee8306bbf8e45e7eca487be5a9a1c477513
Nikhil John <nikhiljohn@Nikhils-MacBook-Pro.local>
2025-09-16 17:12:23 +0100

    Point Firebase config to live project

--- app.js
+++ app.js
@@ -9,17 +9,28 @@ import {
   onSnapshot,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// TODO: replace with your Firebase project settings.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyDMuXqYfxJ_hR9jDh-qDDRIrHnJya1k-dc",
+  authDomain: "gcb-prayer.firebaseapp.com",
+  projectId: "gcb-prayer",
+  storageBucket: "gcb-prayer.firebasestorage.app",
+  messagingSenderId: "836560759878",
+  appId: "1:836560759878:web:6b28043c4863236ba9ae47",
+  measurementId: "G-VJ6NNBFBN8"
 };
 
+
+
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const thanksRef = collection(db, "thanks");
 
