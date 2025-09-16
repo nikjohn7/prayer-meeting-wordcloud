bd586673434bb9438b0add861f8147fc373e24c1
Nikhil John <nikhiljohn@Nikhils-MacBook-Pro.local>
2025-09-16 17:29:23 +0100

    Remove analytics to firebase config

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "firebase/analytics";
+
 import {
   getFirestore,
   collection,
@@ -29,7 +29,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const thanksRef = collection(db, "thanks");
 
